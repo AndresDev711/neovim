ee4ecb42a23e0db3ea3726bbd18555f79711bd92
AndresDev711 <andres7condori@gmail.com>
2026-01-17 08:44:29 -0500

    add lualine colors for path and symbols

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -1,26 +1,32 @@
 ---@type vim.lsp.Config
 return {
-  cmd = { "lua-language-server" },
-  root_markers = {
-    ".luarc.json",
-    ".luarc.jsonc",
-    ".luacheckrc",
-    ".stylua.toml",
-    "stylua.toml",
-    "selene.toml",
-    "selene.yml",
-    ".git",
-  },
-  filetypes = { "lua" },
-  settings = {
-    Lua = {
+	cmd = { "lua-language-server" },
+	root_markers = {
+		".luarc.json",
+		".luarc.jsonc",
+		".luacheckrc",
+		".stylua.toml",
+		"stylua.toml",
+		"selene.toml",
+		"selene.yml",
+		".git",
+	},
+	filetypes = { "lua" },
+	settings = {
+		Lua = {
 			codeLens = {
-        enable = true
-      },
-      hint = {
-        enable = true,
-        semicolon = "Disable"
-      }
-    },
-  },
+				enable = true
+			},
+			hint = {
+				enable = true,
+				semicolon = "Disable"
+			},
+			diagnostics = {
+				globals = { 'vim' }
+			},
+			telemetry = {
+				enable = false
+			}
+		},
+	},
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,26 +1,84 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    require('lualine').setup({
-      sections = {
-        lualine_c = {
-          {
-            'filename',
-            file_status = true,      -- Muestra si el archivo fue modificado
-            path = 1,                -- 0: Solo nombre, 1: Ruta relativa, 2: Ruta absoluta
-            symbols = {
-              modified = ' ●',      -- El icono que aparece al modificar
-              readonly = ' [RO]', 
-              unnamed = '[Sin nombre]',
-            }
-          }
-        }
-      },
-      -- Aquí definimos los colores según el estado
-      options = {
-        theme = 'auto', -- O tu tema favorito (catppuccin, gruvbox, etc.)
-      }
-    })
-  end
+	'nvim-lualine/lualine.nvim',
+	dependencies = { 'nvim-tree/nvim-web-devicons' },
+	opts = {
+		options = {
+			theme = 'auto', -- tema favorito
+			globalstatus = true,
+		},
+		sections = {
+			lualine_c = {
+				{
+					'filename',
+					path = 1,      -- 0:Solo nombre 1:Ruta relativa, 2:Ruta absoluta
+					file_status = true, -- Muestra el archivo modificado
+					fmt = function(name)
+						return '📁 ' .. name
+					end,
+					color = { fg = '#6AEB38' }
+				},
+
+				-- STATUS (modificado / readonly)
+				{
+					function()
+						if vim.bo.modified then
+							return ' ●'
+						end
+						if vim.bo.readonly then
+							return ' [RO]'
+						end
+						return ''
+					end,
+					color = function()
+						if vim.bo.modified then
+							return { fg = '#e0af68' } -- amarillo
+						end
+						if vim.bo.readonly then
+							return { fg = '#f7768e' } -- rojo
+						end
+						return {}
+					end,
+				},
+			}
+		},
+		inactive_winbar = {
+			lualine_c = { "filename" }
+		}
+	},
 }
+
+-- config = function()
+-- 	require('lualine').setup({
+-- 		sections = {
+-- 		},
+-- 		-- Aquí definimos los colores según el estado
+-- 		options = {
+-- 		},
+-- 	})
+-- end
+-- PATH (ruta relativa)
+-- {
+--   function()
+--     local path = vim.fn.expand('%:~:.:h')
+--     if path == '.' or path == '' then
+--       return ''
+--     end
+--     return path .. '/'
+--   end,
+--   color = { fg = '#7aa2f7' }, -- azul
+-- },
+--
+-- -- FILENAME
+-- {
+--   function()
+--     return vim.fn.expand('%:t')
+--   end,
+--   color = { fg = '#c0caf5', gui = 'bold' },
+-- },
+-- 	{
+-- 		symbols = {
+-- 			modified = ' ●', -- Icono visible al modificar
+-- 			readonly = ' [RO]',
+-- 			unnamed = '[Sin nombre]',
+-- 		}
+-- }
